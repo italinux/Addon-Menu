335e4c7dcaa4a62c7f18ffec531678230f6217b8
Matteo Montanari <matteo@italinux.com>
2019-05-26 12:44:29 +0200

    New Features

--- blocks/lazy_menu/templates/fullscreen/view.php
+++ blocks/lazy_menu/templates/fullscreen/view.php
@@ -1,4 +1,26 @@
 <?php
+/**
+.---------------------------------------------------------------------.
+|  @package: Lazy Menu (a.k.a. add-on Menu)
+|  @version: Latest on Github
+|  @link:    http://italinux.com/addon-menu
+|  @docs:    http://italinux.com/addon-menu/docs
+|
+|  @author: Matteo Montanari <matteo@italinux.com>
+|  @link:   http://matteo-montanari.com
+'---------------------------------------------------------------------'
+.---------------------------------------------------------------------------.
+| @copyright (c) 2019                                                       |
+| ------------------------------------------------------------------------- |
+| @license: Concrete5.org Marketplace Commercial Add-Ons & Themes License   |
+|           http://concrete5.org/help/legal/commercial_add-on_license       |
+|           or just: file://lazy_menu/LICENSE.TXT                           |
+|                                                                           |
+| This program is distributed in the hope that it will be useful - WITHOUT  |
+| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     |
+| FITNESS FOR A PARTICULAR PURPOSE.                                         |
+'---------------------------------------------------------------------------'
+*/
 defined('C5_EXECUTE') or die("Access Denied.");
 
 echo $cStyle;
