e94afe5b1fb5cbe04066a88c8f31253abb37ec01
Matteo Montanari <matteo@italinux.com>
2019-05-26 12:25:02 +0200

    New Features

--- blocks/lazy_menu/src/utils.php
+++ blocks/lazy_menu/src/utils.php
@@ -1,26 +1,4 @@
 <?php
-/**
-.---------------------------------------------------------------------.
-|  @package: Lazy Menu (a.k.a. add-on Menu)
-|  @version: v1.0.9 (20 May 2019)
-|  @link:    http://italinux.com/addon-menu
-|  @docs:    http://italinux.com/addon-menu/docs
-|
-|  @author: Matteo Montanari <matteo@italinux.com>
-|  @link:   http://matteo-montanari.com
-'---------------------------------------------------------------------'
-.---------------------------------------------------------------------------.
-| @copyright (c) 2019                                                       |
-| ------------------------------------------------------------------------- |
-| @license: Concrete5.org Marketplace Commercial Add-Ons & Themes License   |
-|           http://concrete5.org/help/legal/commercial_add-on_license       |
-|           or just: file://lazy_menu/LICENSE.TXT                           |
-|                                                                           |
-| This program is distributed in the hope that it will be useful - WITHOUT  |
-| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     |
-| FITNESS FOR A PARTICULAR PURPOSE.                                         |
-'---------------------------------------------------------------------------'
-*/
 namespace Concrete\Package\LazyMenu\Block\LazyMenu\Src;
 
 use Concrete\Core\Page\Page;
