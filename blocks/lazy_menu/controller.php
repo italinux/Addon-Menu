aad4b75b1daa8ff3975aa274942246f07d1fae73
Matteo Montanari <matteo@italinux.com>
2020-01-17 23:14:42 +0800

    New Features

--- blocks/lazy_menu/controller.php
+++ blocks/lazy_menu/controller.php
@@ -616,8 +616,8 @@ class Controller extends BlockController
         );
 
         // Register Assets Animate Configuration
-        $al->register('javascript', $this->getJSelectorId() . '.animate-conf', 'blocks/lazy_menu/jscript/lazy-animate.conf.js?t=' . $this->getSectionId(), $cf, 'lazy_menu');
-        $al->register('javascript-inline', $this->getJSelectorId() . '.animate-init',  '$("section#' . $this->getSectionId()  . '").lazyAnimateInit();', $cf, 'lazy_menu');
+        $al->register('javascript', $this->getJSelectorId() . '.animate-conf', 'blocks/lazy_menu/jscript/lazy-animate.conf.js', $cf, 'lazy_menu');
+        $al->register('javascript-inline', $this->getJSelectorId() . '.animate-init',  '$("section#' . $this->getSectionId()  . '").lazyAnimate(' . $this->getJSelectorBlock() . ');', $cf, 'lazy_menu');
 
         $al->registerGroup(
             'jst.animate.conf', array(
@@ -897,6 +897,11 @@ class Controller extends BlockController
     protected function getJSelectorId()
     {
         return $this->getSectionId() . '.' . self::$btHandlerId;
+    }
+ 
+    protected function getJSelectorBlock()
+    {
+        return str_replace('-', '_', self::$btHandlerId);
     }
 
     protected function getCustomTemplateName()
