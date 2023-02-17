256fbf99010a6883522fa48112081ab2cd36c7c8
Matteo Montanari <matteo@italinux.com>
2023-02-17 21:09:35 +0700

    New Features

--- controller.php
+++ controller.php
@@ -36,7 +36,7 @@ class Controller extends Package
 {
 
     protected $pkgHandle = 'lazy_menu';
-    protected $pkgVersion = '2.2.3';
+    protected $pkgVersion = '2.2.4';
 
     protected $appVersionRequired = '9.0';
 
